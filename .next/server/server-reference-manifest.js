38db091c9d40b47c5d0c641d8e5d7bfb5c377668
Hemanath Peddireddi <Hemanath.Peddireddi@warwick.ac.uk>
2025-02-02 01:42:32 +0000

    a

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jzKO/u4Z2f8aTCYWVhJ7fg5mARwMqRd3UVgRoLtTZ7I=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
